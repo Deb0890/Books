d2ca2e90ef7a6c86cebcdb89597ef34bc4761594
deb0890 <deborahf1@hotmail.com>
2021-07-18 01:28:36 +0100

    begin controllers file, get and get by id

--- mymongo-server/api/controllers/books.js
+++ mymongo-server/api/controllers/books.js
@@ -2,3 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 const Book = require('../models/book')
+
+// books index route
+router.get('/', async (req, res) => {
+    try {
+        const books = await Books.all
+        res.json({books})
+    } catch(err) {
+        res.status(500).json({err})
+    }
+})
+
+// books show route
+router.get('/:id', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        res.json(book)
+    } catch (err) {
+        res.status(404).json({err})
+    }
+})
+
+// books update route
